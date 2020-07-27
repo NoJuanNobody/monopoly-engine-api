11f8a0465b93d57cc1117bb426848a4dd421ae0c
alejandro londono <alejandrolondono@alejandros-MacBook-Pro.local>
2020-07-27 16:22:21 -0400

    creating jail script

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
+// ?info: app level imports 
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+// ?info: monopoly related configuration, like routing and api masthead
 const masthead = require("./public/javascripts/masthead");
 const usersRouter = require('./routes/users')
 const  moveRouter = require('./routes/move')
 const  gameRouter = require('./routes/game')
+
+/*// ?info: db related operations needed to reset db, and initialize libraries needed to spin up docker instance.
+  //?       this functionality is dev only  
+*/
 const { initdb, drop } = require('./db/server');
+
 var app = express();
 
-
+// ?info: app configuration
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
